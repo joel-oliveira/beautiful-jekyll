9189ef7d2e0c39dbbe9c72cfff702fbd77acf070
Joel Oliveira <joliveira@tillster.com>
2016-06-25 18:01:08 +0100

    Triangle update.

--- js/triangle.js
+++ js/triangle.js
@@ -1,7 +1,7 @@
 triangle = null;
 
 function Triangle() {
-	canvasDiv = document.getElementById("canvas");
+	canvasDiv = $("#canvas").get(0);
 	this.canvas = Raphael(canvasDiv, 200, 200);
 }
 
@@ -33,44 +33,45 @@ Triangle.prototype.getY = function(y) {
 Triangle.prototype.getApex = function() {
 	//via http://mathforum.org/library/drmath/view/51836.html
 	var b2 = Math.pow(this.scaledSide2, 2);
-	var deltaX = parseInt((Math.pow(this.scaledSide1, 2) + Math.pow(this.scaledSide2, 2) - Math.pow(this.scaledSide3, 2))/(2*this.scaledSide1));
+	var deltaX = parseInt((Math.pow(this.scaledSide1, 2) + Math.pow(this.scaledSide2, 2) - Math.pow(this.scaledSide3, 2)) / (2 * this.scaledSide1));
 	var apexX = this.x1 + deltaX;
 	var apexY = this.y3 - Math.sqrt(Math.abs(b2 - Math.pow(deltaX, 2)));
-	return [apexX,apexY];
+	return [apexX, apexY];
 }
 
-Triangle.prototype.getTriangleType = function() {
-    var triangleType = "Isosceles";
+Triangle.prototype.getType = function() {
+    var type = "Isosceles";
 	if (this.sorted_sides[2] > this.sorted_sides[0] + this.sorted_sides[1]) {
-		triangleType = "Invalid";
+		type = "Invalid";
 	} else if (this.sorted_sides[2] == this.sorted_sides[0] + this.sorted_sides[1]) {
-		triangleType =  "Degenerate";
+		type =  "Degenerate";
 	} else if ((this.sorted_sides[0] == this.sorted_sides[1]) && (this.sorted_sides[1] == this.sorted_sides[2])) {
-		triangleType =  "Equilateral";
+		type =  "Equilateral";
 	} else if (Math.pow(this.sorted_sides[2], 2) == (Math.pow(this.sorted_sides[0], 2) + (Math.pow(this.sorted_sides[1], 2)))) {
-		triangleType =  "Right";
+		type =  "Right";
 	} else if ((this.sorted_sides[2] != this.sorted_sides[1]) && (this.sorted_sides[1] != this.sorted_sides[0])) {
-		triangleType =  "Scalene";
+		type =  "Scalene";
 	}
-	return triangleType;
+	return type;
 }
 
 Triangle.prototype.draw = function() {
 	this.canvas.clear();
-	this.triangleType = this.getTriangleType();
+	this.type = this.getType();
 	if (this.triangleType != "Invalid") {
 		this.apexPoint = this.getApex();
 	    this.x2 = parseInt(this.apexPoint[0]);
 	    this.y2 = parseInt(this.apexPoint[1]);
-		this.center();
-		this.triangleGraphic = this.canvas.path({stroke: "#036"}).moveTo(this.x1, this.y1).lineTo(this.x3, this.y3).lineTo(this.x2, this.y2).moveTo(this.x2, this.y2).lineTo(this.x1, this.y1).andClose();
-		this.triangleGraphic.attr("fill", "#6699bb");
-		document.getElementById('triangle_coordinates').value = "(" + this.x1 + "," + this.y1 + ") (" + this.x2 + "," + this.y2 + ") (" + this.x3 + "," + this.y3 + ")";
+		if ($.isNumeric(this.x2) && $.isNumeric(this.y2)) {
+			this.center();
+			this.triangleGraphic = this.canvas.path("M " + this.x1 + "," + this.y1 + " L " + this.x3 + "," + this.y3 + " L " + this.x2 + "," + this.y2 + " L " + this.x1 + "," + this.y1 + " Z");			
+			this.triangleGraphic.attr("stroke", "#036");
+			this.triangleGraphic.attr("fill", "#6699bb");
+		}
+		this.coordinates = "(" + this.x1 + "," + this.y1 + ") (" + this.x2 + "," + this.y2 + ") (" + this.x3 + "," + this.y3 + ")";
 	} else {
-		document.getElementById('triangle_coordinates').value = "";
+		this.coordinates = "";
 	}
-	document.getElementById('triangle_type').innerHTML = "\n" + this.triangleType + "\n";
-	document.getElementById('triangle_category').value = this.triangleType;
 }
 
 Triangle.prototype.center = function() {
@@ -84,29 +85,95 @@ Triangle.prototype.center = function() {
 	this.x3 = this.x3 + shiftBy;
 }
 
-function sortNumber(a,b) {
+function sortNumber(a, b) {
 	return a - b;
 }
 
-function drawTriangle() {
-	var side1val = document.getElementById('triangle_side1').value;
-	var side2val = document.getElementById('triangle_side2').value;
-	var side3val = document.getElementById('triangle_side3').value;
+function drawTriangle(skipAddToList) {
+	var side1val = $("#side1").val();
+	var side2val = $("#side2").val();
+	var side3val = $("#side3").val();
 	triangle.setSides(side1val, side2val, side3val);
 	triangle.draw();
-	document.getElementById('triangles_list').innerHTML = '<div class="triangle_row clearfloat"><div class="triangle_data_cell narrow">' + triangle.s1 + '</div><div class="triangle_data_cell narrow">' + triangle.s2 + '</div><div class="triangle_data_cell narrow">' + triangle.s3 + '</div><div class="triangle_data_cell wide">' + triangle.triangleType + '</div><div class="triangle_data_cell wide">' + document.getElementById('triangle_coordinates').value + '</div></div>' + document.getElementById('triangles_list').innerHTML;
+	$("#type").html("\n" + triangle.type + "\n");
+	if(!skipAddToList) {
+		addToList(triangle.s1, triangle.s2, triangle.s3, triangle.type, triangle.coordinates);
+	}
+	var list = $.jStorage.get("list");
+	if(!list) {
+		list = new Array();
+	}
+	list.push([triangle.s1, triangle.s2, triangle.s3, triangle.type, triangle.coordinates]);
+	$.jStorage.set("list", list);
+}
+
+function addToList(s1, s2, s3, type, coordinates) {
+	$("#list").prepend("<div class=\"triangle_row clearfloat\"><div class=\"triangle_data_cell narrow\">" + s1 + "</div><div class=\"triangle_data_cell narrow\">" + s2 + "</div><div class=\"triangle_data_cell narrow\">" + s3 + "</div><div class=\"triangle_data_cell wide\">" + type + "</div><div class=\"triangle_data_cell wide\">" + coordinates + "</div></div>");
 }
 
 function initializeTriangle() {
 	triangle = new Triangle();
 }
 
-window.onload=function(){ initializeTriangle(); }
+function resetSession() {
+	$("#list").html("");
+	$.jStorage.flush();
+	return ($("#web").val() == 1);
+}
 
-function inspect(thing) {
-	var serialized_thing = "";
-	for (bit in thing) {
-		serialized_thing += bit + ": " + thing[bit] + "\n";
+function setBehavior(web) {
+	switch(web) {
+		case "1":
+			$("#form").off("submit");
+			$("input[type='submit']").removeClass("hide");
+			$("input[id^='side']").off("change");
+			break;
+		case "2":
+			$("#form").off("submit");
+			$("input[type='submit']").removeClass("hide");
+			$("#form").on("submit", function(event) {
+				event.preventDefault();
+				$.get($("#form").attr("action"), {web: $('#web').val(), side1: $('#side1').val(), side2: $('#side2').val(), side3: $('#side3').val()} );
+				drawTriangle();
+			});
+			$("input[id^='side']").off("change");
+			break;
+		case "3":
+			$("#form").off("submit");
+			$("input[type='submit']").addClass("hide");
+			$("#form").on("submit", function(event) {
+				event.preventDefault();
+			});
+			$("input[id^='side']").on("change", function() {
+				drawTriangle();
+			});
+			break;
+		default:
+			//default code block
 	}
-	return (serialized_thing);
+}
+
+window.onload = function() {
+	initializeTriangle();
+	$('#reset').click(function(){ return resetSession(); });
+	var url = $.url();
+	var draw = false;
+	var web = parseInt(Number(escape(url.param("web"))));
+	if((web > 0) && (web < 4)) {
+		$("#web").val(web);
+		setBehavior("" + web);
+	}
+	for (var i = 1; i < 4; i++) {
+		$("#side" + i).val(url.param("side" + i));
+		draw = (draw || $.isNumeric($("#side" + i).val()));
+	}
+	if (draw) {
+		drawTriangle(true);
+	}
+	var list = $.jStorage.get("list");
+	if(list) {
+		for (var l = 0; l < list.length; l++) {
+			addToList(list[l][0], list[l][1], list[l][2], list[l][3], list[l][4]);
+		}
+	}
 }
--- triangles.md
+++ triangles.md
@@ -5,52 +5,55 @@ subtitle: A classic testing exercise
 css:
   - "/css/triangle.css"
 js:
-  #- "/js/raphael-2.2.0.min.js" # "https://raw.githubusercontent.com/DmitryBaranovskiy/raphael/master/raphael.min.js"
-  - "/js/raphael.js"
+  - "/js/raphael-2.2.0.min.js"
+  - "/js/purl-2.3.1.min.js"
+  - "/js/json2-20150503.min.js"
+  - "/js/jstorage-0.4.12.min.js"
   - "/js/triangle.js"
 ---
-
-Inspired on the work done by [Elisabeth Hendrickson](http://testobsessed.com/2007/03/testing-triangles-a-classic-exercise-updated-for-the-web/) available [here](http://practice.agilistry.com/triangle).
-
 <div id="triangle">
 	<div id="triangle_form">
-		<form action="/triangles" method="post" onsubmit="return false;">
-			<p>
-				<i>Please enter sizes for the sides of the triangle:</i>
-			</p>
-			<input id="triangle_category" name="triangle[category]" type="hidden" />
-			<input id="triangle_coordinates" name="triangle[coordinates]" type="hidden" />
+		<form id="form" method="get" action="">
+			<i>Please enter sizes for the sides:</i>
 			<div id="triangle_frame">
-				<div id="side2_container" class="side">
+				<div class="side side2">
 					Side 2<br />
-					<input id="triangle_side2" name="triangle[side2]" onchange="drawTriangle();" size="5" type="text" />
+					<input id="side2" name="side2" size="5" type="text" />
 				</div>
-				<div id="side3_container" class="side">
+				<div class="side side3">
 					Side 3<br />
-					<input id="triangle_side3" name="triangle[side3]" onchange="drawTriangle();" size="5" type="text" />
+					<input id="side3" name="side3" size="5" type="text" />
 				</div>
-				<div id="side1_container" class="side">
+				<div class="side side1">
 					Side 1<br />
-					<input id="triangle_side1" name="triangle[side1]" onchange="drawTriangle();" size="5" type="text" />
+					<input id="side1" name="side1" size="5" type="text" />
 				</div>
-				<div id="triangle_type">
+				<div id="type">
 				</div>
 				<div id="canvas">&nbsp;</div>
 			</div>
+			<br />
+			Web: <select id="web" name="web" onchange="setBehavior(this.value)">
+				<option value="1" selected="selected">1.0</option>
+				<option value="2">2.0</option>
+				<option value="3">3.0</option>
+			</select><input type="submit" class="submit" tabindex="-1" value="Draw Triangle">
 		</form>
+		<p>Inspired on the work done by <a href="http://testobsessed.com/2007/03/testing-triangles-a-classic-exercise-updated-for-the-web/">Elisabeth Hendrickson</a> available <a href="http://practice.agilistry.com/triangle">here</a>.</p>
 	</div>
 	<div id="triangle_history">
-	<p><i>Test Data History<br />(for this test session)</i></p>
-	<b>
-		<div class="triangle_row">
-			<div class="triangle_data_cell narrow">Side 1</div>
-			<div class="triangle_data_cell narrow">Side 2</div>
-			<div class="triangle_data_cell narrow">Side 3</div>
-			<div class="triangle_data_cell wide">Category</div>
-			<div class="triangle_data_cell wide">Coordinates</div>
-		</div></b>
+		<i>Test Data History (<a href="/triangles" id="reset">reset test session</a>)</i>
+		<b>
+			<div class="triangle_row">
+				<div class="triangle_data_cell narrow">Side 1</div>
+				<div class="triangle_data_cell narrow">Side 2</div>
+				<div class="triangle_data_cell narrow">Side 3</div>
+				<div class="triangle_data_cell wide">Category</div>
+				<div class="triangle_data_cell wide">Coordinates</div>
+			</div>
+		</b>
 		<br class="clearfloat" />
-		<div id="triangles_list">
+		<div id="list">
 		</div>
 	</div>
 </div>
